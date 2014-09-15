72ce258370fde78148cbc5a78352ec39827079d1
wooorm <tituswormer@gmail.com>
2014-09-15 11:11:36 +0200

    0.0.1

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * The constants as defined by the revised Coleman-Liau index.
+ */
+
+var LETTER_WEIGHT = 0.0588,
+    SENTENCE_WEIGHT = 0.296,
+    BASE = 15.8,
+    PERCENTAGE = 100;
+
+/**
+ * Get the grade level of a given value according to the Coleman-Liau index.
+ * More information is available at WikiPedia:
+ *
+ *   http://en.wikipedia.org/wiki/Coleman–Liau_index
+ *
+ * @param {Object} counts
+ * @param {number} counts.word - Number of words.
+ * @param {number} counts.sentence - Number of sentences.
+ * @param {number} counts.letter - Number of letters (incl. digits).
+ * @return {number}
+ */
+
+function colemanLiau(counts) {
+    if (!counts || !counts.sentence || !counts.word || !counts.letter) {
+        return NaN;
+    }
+
+    return LETTER_WEIGHT * (counts.letter / counts.word * PERCENTAGE) -
+        SENTENCE_WEIGHT * (counts.sentence / counts.word * PERCENTAGE) -
+        BASE;
+}
+
+/**
+ * Export `colemanLiau`.
+ */
+
+module.exports = colemanLiau;
---
+++ spec/coleman-liau.spec.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * Module dependencies (colemanLiau, assert).
+ */
+
+var colemanLiau = require('..'),
+    assert = require('assert');
+
+/**
+ * Assert, but up to 6 digits.
+ */
+
+function roundAssert(a, b) {
+    assert(Math.round(a * 1000000) === Math.round(b * 1000000));
+}
+
+/**
+ * Unit tests.
+ */
+
+describe('colemanLiau()', function () {
+    it('should be of type `function`', function () {
+        assert(typeof colemanLiau === 'function');
+    });
+
+    it('should work', function () {
+        var result;
+
+        /**
+         * Return NaN when an invalid value is given.
+         */
+
+        result = colemanLiau();
+
+        assert(result !== result);
+
+        /**
+         * Existing computer programs that measure readability are based
+         * largely upon subroutines which estimate number of syllables,
+         * usually by counting vowels. The shortcoming in estimating
+         * syllables is that it necessitates keypunching the prose into
+         * the computer. There is no need to estimate syllables since word
+         * length in letters is a better predictor of readability than word
+         * length in syllables. Therefore, a new readability formula was
+         * computed that has for its predictors letters per 100 words and
+         * sentences per 100 words. Both predictors can be counted by an
+         * optical scanning device, and thus the formula makes it
+         * economically feasible for an organization such as the U.S.
+         * Office of Education to calibrate the readability of all textbooks
+         * for the public school system.
+         *
+         * Sentences: 5
+         * Words: 119
+         * Syllables: 639
+         */
+
+        roundAssert(colemanLiau({
+            'sentence' : 5,
+            'word' : 119,
+            'letter' : 639
+        }), 14.53042);
+    });
+});
