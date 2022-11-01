19a36e1f6aa303337b7b94ba15b829b6df974d52
Titus Wormer <tituswormer@gmail.com>
2022-11-01 15:31:39 +0100

    Add docs to types

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
+/**
+ * @typedef Counts
+ *   Counts from input document.
+ * @property {number} sentence
+ *   Number of sentences.
+ * @property {number} word
+ *   Number of words.
+ * @property {number} letter
+ *   Number of letters.
+ */
+
+/**
+ * @typedef {Counts} ColemanLiauCounts
+ *   Deprecated: please use the `Counts` type instead.
+ */
+
 const letterWeight = 0.0588
 const sentenceWeight = 0.296
 const base = 15.8
 const percentage = 100
 
 /**
- * @typedef {Object.<string, number>} ColemanLiauCounts
- * @propert {number} sentence
- * @propert {number} word
- * @propert {number} letter
- */
-
-/**
- * Given an object containing the number of words (`word`), the number of sentences (`sentence`), and the number of letters  (`letter`) in a document, returns the grade level associated with the document.
+ * Given an object containing the number of words (`word`), the number of
+ * sentences (`sentence`), and the number of letters  (`letter`) in a
+ * document, returns the grade level associated with the document.
  *
- * @param {ColemanLiauCounts} counts
+ * @param {Counts} counts
+ *   Counts from input document.
  * @returns {number}
+ *   Grade level associated with the document.
  */
 export function colemanLiau(counts) {
   if (!counts || !counts.sentence || !counts.word || !counts.letter) {
